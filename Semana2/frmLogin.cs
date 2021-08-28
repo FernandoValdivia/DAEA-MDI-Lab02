dbd924e41bef1d3c9886b542143f9bd8176d6c77
FernandoValdivia <luis.valdivia.m@tecsup.edu.pe>
2021-08-28 00:21:10 -0500

    Agregar archivos de proyecto.

---
+++ Semana2/frmLogin.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Semana2
+{
+    public partial class frmLogin : Form
+    {
+        public frmLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void btnIniciar_Click(object sender, EventArgs e)
+        {
+            string[] users = { "lvaldivia", "fmurillo", "fernando01" };
+
+            string usuario = "lvaldivia";
+            string pass = "123456";
+            if (txtUsuario.Text.Equals(usuario) && txtPassword.Text.Equals(pass))
+            {
+                PrincipalMDI principal = new PrincipalMDI();
+                principal.Show();
+                this.Hide();
+            } else {
+                MessageBox.Show("Credenciales incorrectas","Validación");
+            }
+            
+        }
+
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
